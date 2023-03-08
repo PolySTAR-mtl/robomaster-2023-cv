7114a3970e9ba8d30bf37a3c91b2e996905b812c
Sebastien Darche <sebastien.darche@polymtl.ca>
2023-03-08 16:35:37 -0500

    Implement cinematic model for odometry, stub for integration (missing rotation)

--- ros_ws/src/locate/include/odom.hpp
+++ ros_ws/src/locate/include/odom.hpp
@@ -8,6 +8,8 @@
 
 #include "ros/ros.h"
 
+#include "Eigen/Core"
+
 #include "nav_msgs/Odometry.h"
 
 class Odom {
@@ -17,10 +19,23 @@ class Odom {
         pub_speed = nh.advertise<nav_msgs::Odometry>("odom_speed", 1);
     }
 
-    void handlePos(float enc1, float enc2, float enc3, float enc4);
+    void handlePos(float enc1, float enc2, float enc3, float enc4, double dt);
     void handleSpeed(float v1, float v2, float v3, float v4);
 
-  public:
+    Eigen::Vector3d cinematic(Eigen::Vector4d& wheel_speed);
+
+    Eigen::Vector3d integrate(Eigen::Vector3d& robot_speed, double dt);
+
+  private:
     ros::NodeHandle nh;
     ros::Publisher pub_pos, pub_speed;
+
+    nav_msgs::Odometry last_estimation;
+
+    double wheel_radius;
+    double length_x, length_y;
+
+    Eigen::Vector4d last_enc;
+    Eigen::Vector4d last_speed;
+    double smooth_factor = 0.95;
 };
--- ros_ws/src/locate/src/odom.cpp
+++ ros_ws/src/locate/src/odom.cpp
@@ -6,6 +6,39 @@
 
 #include "odom.hpp"
 
-void Odom::handlePos(float enc1, float enc2, float enc3, float enc4) {}
+void Odom::handlePos(float enc1, float enc2, float enc3, float enc4,
+                     double dt) {
+    Eigen::Vector4d enc(enc1, enc2, enc3, enc4);
+    auto speed = enc - last_enc; // TODO : figure out direction!
+
+    Eigen::Vector4d speed_smooth =
+        smooth_factor * speed + (1 - smooth_factor) * last_speed;
+
+    auto robot_vel = cinematic(speed_smooth);
+    auto pos = integrate(robot_vel, dt);
+
+    last_enc = enc;
+    last_speed = speed;
+}
+
+Eigen::Vector3d Odom::cinematic(Eigen::Vector4d& vel) {
+    auto v_x = (vel[0] + vel[1] + vel[2] + vel[3]) * wheel_radius / 4.;
+
+    auto v_y =
+        (-1. * vel[0] + vel[1] + vel[2] + -1. * vel[3]) * wheel_radius / 4.;
+
+    auto omega = (-1. * vel[0] + vel[1] + -1 * vel[2] + vel[3]) * wheel_radius /
+                 (4. * (length_x + length_y));
+
+    return {v_x, v_y, omega};
+}
+
+Eigen::Vector3d Odom::integrate(Eigen::Vector3d& speed, double dt) {
+    auto x = last_estimation.pose.pose.position.x + speed[0] * dt;
+    auto y = last_estimation.pose.pose.position.y + speed[1] * dt;
+    auto r = 0.; // TODO convert quaternion to euler
+
+    return {x, y, r};
+}
 
 void Odom::handleSpeed(float v1, float v2, float v3, float v4) {}
