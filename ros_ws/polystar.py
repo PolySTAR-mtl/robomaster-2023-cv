5725149d1450f9f0794a434ec96e91393e16a9b5
Sebastien Darche <sebastien.darche@free.fr>
2023-07-05 12:16:47 -0700

    Argparse boilerplate

--- ros_ws/polystar.py
+++ ros_ws/polystar.py
@@ -13,37 +13,93 @@
 """
 
 import argparse
+import sys
+
+# Parser
+
+parser = argparse.ArgumentParser()
+subparser = parser.add_subparsers(dest='command', help='Command to run')
+subparser.add_parser('enable', help='Enable daemon')
+subparser.add_parser('disable', help='Disable daemon')
+subparser.add_parser('start', help='Manually start daemon')
+subparser.add_parser('kill', help='Manually kill daemon')
+subparser.add_parser('restart', help='Manually restart daemon')
+subparser.add_parser('run', help='Run pipeline')
+subparser.add_parser('stop', help='Stop pipeline')
+
+set_enemy_parser = subparser.add_parser('set-enemy', help='Set enemy color')
+set_enemy_parser.add_argument('color', choices=['blue', 'red'], help='Enemy color')
+
+shoot_parser = subparser.add_parser('shoot')
+shoot_parser.add_argument('val', choices=['on', 'off'])
+
+# Commands
 
 def is_daemon_running():
     pass
 
 def enable():
-    pass
+    print('Enabling daemon')
+    print('Success')
 
 def disable():
-    pass
+    print('Disabling daemon')
+    print('Success')
 
 def start():
-    pass
+    print('Starting daemon')
+    print('Success')
 
 def kill():
-    pass
+    print('Killing daemon')
+    print('Success')
 
 def restart():
     kill()
     start()
 
 def run():
-    pass
+    print('Running the pipeline')
+
+    print('Success')
 
 def stop():
     """
     Stops the pipeline (running, but idle)
     """
+    print('Stopping the pipeline')
+
+    print('Success')
+
+def set_enemy(color):
+    pass
+
+def shoot(val):
     pass
 
 def main():
-    pass
+    args = parser.parse_args()
+    if args.command == 'enable':
+        enable()
+    elif args.command == 'disable':
+        disable()
+    elif args.command == 'start':
+        start()
+    elif args.command == 'kill':
+        kill()
+    elif args.command == 'restart':
+        restart()
+    elif args.command == 'run':
+        run()
+    elif args.command == 'stop':
+        stop()
+    elif args.command == 'set-enemy':
+        set_enemy(args.color)
+    elif args.command == 'shoot':
+        shoot(args.val)
+    else:
+        print(f'Unsupported command "{args.command}"', file=sys.stderr)
+        sys.exit(-1)
 
 if __name__ == "__main__":
     main()
