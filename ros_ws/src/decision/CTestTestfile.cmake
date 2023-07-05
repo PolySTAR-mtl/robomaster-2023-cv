c51c2711ac18bf571f9b469a99941083bd590493
Sebastien Darche <sebastien.darche@free.fr>
2023-07-05 11:15:12 -0700

    Clean-up decision (CMake files?)

--- ros_ws/src/decision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ozone/robomaster-2023-cv/ros_ws/src/decision
-# Build directory: /home/ozone/robomaster-2023-cv/ros_ws/src/decision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
