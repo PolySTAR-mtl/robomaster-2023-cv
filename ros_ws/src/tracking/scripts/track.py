99a686cfc166a498b55a3ada39801e6a7d30bfd0
Sébastien Darche <sebastien.darche@polymtl.ca>
2023-07-06 16:48:02 -0400

    Less verbose output from tracker

--- ros_ws/src/tracking/scripts/track.py
+++ ros_ws/src/tracking/scripts/track.py
@@ -50,15 +50,13 @@ class Tracking:
 
     def detections_callback(self, detections_ros: Detections) -> Tracklets:
         self.no_detection = False
-        length = len(detections_ros.detections)
-        rospy.loginfo('\n' + f' Detections ({length}) '.center(30, '='))
         class2detections = defaultdict(list) # Keys are classes (c)
         class2confs = defaultdict(list)      # Keys are classes (c)
+
         for detection_ros in detections_ros.detections:
-            rospy.loginfo(f'\n{detection_ros}')
             detection, clss = self.det_ros2mot(detection_ros)
             class2detections[clss].append(detection)
-        
+
         tracklets = []
         for clss in set(self.mots.keys()).union(class2detections.keys()):
             detections = class2detections[clss]
@@ -67,15 +65,11 @@ class Tracking:
             for track in tracks:
                 tracklet = self.trk_mot2ros(track=track, clss=clss)
                 tracklets.append(tracklet)
-        
-        length = len(tracklets)
-        rospy.loginfo('\n' + f' Tracklets ({length}) '.center(30, '='))
-        for tracklet in tracklets:
-            rospy.loginfo(f'\n{tracklet}')
+
         tracklets = Tracklets(tracklets=tracklets)
         self.pub.publish(tracklets)
         return tracklets
-        
+
     def det_ros2mot(self, detection: DetectionROS) -> DetectionMOT:
         box = np.array([detection.x, detection.y, 
                         detection.x + detection.w, detection.y + detection.h])
