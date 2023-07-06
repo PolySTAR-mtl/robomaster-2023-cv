5b74bf88ce4d59be45e795410949b2e44c3cca8f
Sébastien Darche <sebastien.darche@polymtl.ca>
2023-07-06 11:51:07 -0400

    Install script

--- install.sh
+++ install.sh
@@ -2,5 +2,11 @@
 
 # Installs the necessary scripts/config files. Binary will remain in the repo folder
 
+# Control script
+mkdir -p ~/.local/bin/
 ln -s $(pwd)/polystar.py ~/.local/bin/polystar
-cp polystar.service /etc/systemd/user/
+
+# Systemd service
+mkdir -p ~/.config/systemd/user/
+ln -s $(pwd)/polystar.service ~/.config/systemd/user/polystar.service
+
